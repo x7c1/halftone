b2e216f9be13ee2bf7c43e2889b9469d44cde3d2
x7c1 <jmtaro+x7c1@gmail.com>
2021-02-24 03:40:07 +0900

    add react sample

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -1,15 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const paths = (() => {
+    const root = path.resolve(__dirname, '..')
+    const ui = path.resolve(root, 'src-ui')
+    return {root, ui}
+})()
+
 module.exports = {
-    entry: './src-ui/index.js',
+    entry: path.resolve(paths.ui, 'index.js'),
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, '..', 'dist'),
+        path: path.resolve(paths.root, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            '@babel/transform-react-jsx',
+                        ],
+                    },
+                },
+            }
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'src-ui/index.html',
-        })
+            template: path.resolve(paths.ui, 'index.html'),
+        }),
     ]
 };
--- src-ui/index.js
+++ src-ui/index.js
@@ -1,1 +1,10 @@
-alert("hello")
+import ReactDOM from 'react-dom';
+
+const name = 'Halftone';
+
+const element = <h1>Hello, {name}!</h1>;
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
