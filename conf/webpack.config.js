ff2ea02e0f6f69b66d3d08eba5f92beddcfb7f84
x7c1 <jmtaro+x7c1@gmail.com>
2021-02-27 15:07:29 +0900

    prettier --write

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -1,38 +1,36 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const paths = (() => {
-    const root = path.resolve(__dirname, '..')
-    const ui = path.resolve(root, 'src-ui')
-    return {root, ui}
-})()
+  const root = path.resolve(__dirname, '..');
+  const ui = path.resolve(root, 'src-ui');
+  return { root, ui };
+})();
 
 module.exports = {
-    mode: 'production',
-    entry: path.resolve(paths.ui, 'index.js'),
-    output: {
-        filename: 'main.js',
-        path: path.resolve(paths.root, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        plugins: [
-                            '@babel/transform-react-jsx',
-                        ],
-                    },
-                },
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(paths.ui, 'index.html'),
-        }),
-    ]
+  mode: 'production',
+  entry: path.resolve(paths.ui, 'index.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(paths.root, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['@babel/transform-react-jsx'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(paths.ui, 'index.html'),
+    }),
+  ],
 };
--- src-ui/index.js
+++ src-ui/index.js
@@ -5,7 +5,4 @@ const name = 'Halftone';
 
 const element = <h1>Hello, {name}!</h1>;
 
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+ReactDOM.render(element, document.getElementById('root'));
